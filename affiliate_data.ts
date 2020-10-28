102d0503997ea3eb7dbb7cb9460ffc94e45806ff
Brice Wilson <brice@5wilsons.net>
2020-10-28 12:18:20 -0400

    added two modules for use in m4

---
+++ affiliate_data.ts
@@ -0,0 +1,6 @@
+export function getDataUrl(affiliateID: number) {
+  switch (affiliateID) {
+    case 5:
+      return 'https://raw.githubusercontent.com/bricewilson/Deno-Getting-Started/Using_the_Deno_Runtime_API/products.json';
+  }
+}
