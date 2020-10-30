93abeb47f42fea10a30f0f5883c39065764f4f35
Brice Wilson <brice@5wilsons.net>
2020-10-30 17:27:21 -0400

    recorded demo for clip 1

---
+++ deps.ts
@@ -0,0 +1,2 @@
+export { getDataUrl } from 'https://raw.githubusercontent.com/bricewilson/Deno-Getting-Started/Linking_to_External_Code/affiliate_data.ts';
+export { getThresholdPrice } from 'https://raw.githubusercontent.com/bricewilson/Deno-Getting-Started/Linking_to_External_Code/pricing_rules.ts';
--- process_affiliate_data.ts
+++ process_affiliate_data.ts
@@ -1,8 +1,15 @@
 import { Product } from './product.ts';
+import { getDataUrl, getThresholdPrice } from './deps.ts';
+
+// import { getDataUrl } from 'data_sources';
+// import { getThresholdPrice } from 'prices';
+// import { getDataUrl } from 'https://raw.githubusercontent.com/bricewilson/Deno-Getting-Started/Linking_to_External_Code/affiliate_data.ts';
+// import { getThresholdPrice } from 'https://raw.githubusercontent.com/bricewilson/Deno-Getting-Started/Linking_to_External_Code/pricing_rules.ts';
+
 
 const affiliateID: number = parseInt(Deno.args[0]);
 
-const data_url: string = 'https://raw.githubusercontent.com/bricewilson/Deno-Getting-Started/Using_the_Deno_Runtime_API/products.json';
+const data_url: string = getDataUrl(affiliateID);
 
 const response = await fetch(data_url);
 
@@ -10,8 +17,10 @@ const data = await response.json() as Product[];
 
 const affiliate_products: Product[] = new Array<Product>();
 
+const threshold_price: number = getThresholdPrice(affiliateID);
+
 data.forEach(product => {
-  if(product.price > 40) {
+  if(product.price > threshold_price) {
     product.affiliateID = affiliateID;
     affiliate_products.push(product);
   }
